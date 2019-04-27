6ec689c106adea3bc0880e23668fc52f2f607609
Jessica Culver <jculver1900@gmail.com>
2019-04-27 13:18:12 -0600

    Update programs.js

--- src/components/programs.js
+++ src/components/programs.js
@@ -15,7 +15,7 @@ return(
             <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
               <img
                 className="img-fluid"
-                src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg"
+                src="./classroom.jpg"
                 alt=""
               />
               <a href="#!">
@@ -36,9 +36,6 @@ return(
             </p>
           </MDBCol>
         </MDBRow>
-        <hr className="my-5" />
-        <MDBRow>
-        </MDBRow>
       </MDBCardBody>
     </MDBCard>
 
@@ -49,7 +46,7 @@ return(
             <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
               <img
                 className="img-fluid"
-                src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg"
+                src="./tunnelbaby.jpg"
                 alt=""
               />
               <a href="#!">
@@ -72,9 +69,6 @@ return(
             </p>
           </MDBCol>
         </MDBRow>
-        <hr className="my-5" />
-        <MDBRow>
-        </MDBRow>
       </MDBCardBody>
     </MDBCard>
 
@@ -85,7 +79,7 @@ return(
             <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
               <img
                 className="img-fluid"
-                src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg"
+                src="boyhug.jpg"
                 alt=""
               />
               <a href="#!">
@@ -106,12 +100,107 @@ return(
             </p>
           </MDBCol>
         </MDBRow>
-        <hr className="my-5" />
+      </MDBCardBody>
+    </MDBCard>
+
+    <MDBCard className="my-5 px-5 pb-5">
+      <MDBCardBody>
         <MDBRow>
+          <MDBCol lg="5">
+            <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
+              <img
+                className="img-fluid"
+                src="./class2.jpg"
+                alt=""
+              />
+              <a href="#!">
+                <MDBMask overlay="white-slight" />
+              </a>
+            </MDBView>
+          </MDBCol>
+          <MDBCol lg="7">
+            <h3 className="font-weight-bold mb-3 p-0">
+              <strong>School Age Program</strong>
+            </h3>
+            <p>
+            For today's busy parents,
+            finding a safe, enriching environment where
+            children can spend time before or after school is a
+            big concern. We understand your needs and we
+            deliver a fun, flexible, and learning-focused
+            program. We currently server children attending
+            Carson, Lowry, McMeen, and Palmer Elementary.
+            Please contact us about possibly adding your
+            child's school. For more information please
+            contact Shirley Koeller at 
+            <a href="shirley@augustanadenver"> shirley@augustanadenver</a>
+            .
+            </p>
+          </MDBCol>
         </MDBRow>
       </MDBCardBody>
     </MDBCard>
 
+    <MDBCard className="my-5 px-5 pb-5">
+      <MDBCardBody>
+        <MDBRow>
+          <MDBCol lg="5">
+            <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
+              <img
+                className="img-fluid"
+                src="sandboy.jpg"
+                alt=""
+              />
+              <a href="#!">
+                <MDBMask overlay="white-slight" />
+              </a>
+            </MDBView>
+          </MDBCol>
+          <MDBCol lg="7">
+            <h3 className="font-weight-bold mb-3 p-0">
+              <strong>Art Program</strong>
+            </h3>
+            <p>
+            Our art activities enable children to
+            explore a variety of media and experiment with
+            color, design and construction.
+            </p>
+          </MDBCol>
+        </MDBRow>
+      </MDBCardBody>
+    </MDBCard>
+
+    <MDBCard className="my-5 px-5 pb-5">
+      <MDBCardBody>
+        <MDBRow>
+          <MDBCol lg="5">
+            <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
+              <img
+                className="img-fluid"
+                src="bicycleboy.jpg"
+                alt=""
+              />
+              <a href="#!">
+                <MDBMask overlay="white-slight" />
+              </a>
+            </MDBView>
+          </MDBCol>
+          <MDBCol lg="7">
+            <h3 className="font-weight-bold mb-3 p-0">
+              <strong>Summer Camp</strong>
+            </h3>
+            <p>
+            Need a safe and fun place for the
+            kids this summer? Join Summer Explorers! An
+            exciting summer is being planned to entertain the
+            kids and to make it a summer to remember. For
+            more information contact Shirley Koeller at 
+            <a href="shirley@augustanadenver.org"> shirley@augustanadenver.org </a>
+            </p>
+          </MDBCol>
+        </MDBRow>
+      </MDBCardBody>
+    </MDBCard>
     
     </div>
 )
@@ -119,3 +208,4 @@ return(
 }
 
 export default ProgramsPage;
+
