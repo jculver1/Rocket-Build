e4e6656f93699528102395bbb873e4a8b93615e0
Jessica Culver <jculver1900@gmail.com>
2019-04-27 10:25:59 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavbarPage from './components/nav.js';
-
+import FooterPage from './components/footer'
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavbarPage />
+         <FooterPage/>
       </div>
     )
   }
